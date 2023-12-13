e9346d59b73a1c789fa6e3418c13e92451843815
GeorgiosTE <gteleftheriou@gmail.com>
2023-12-13 15:58:52 +0200

    no message

--- BaratheonFamilyTree/src/App.java
+++ BaratheonFamilyTree/src/App.java
@@ -6,48 +6,57 @@ enum Gender {
 class Node {
     private String name;
     private Gender gender;
-    private Node Parent;
+    private Node[] Parents;
     private ArrayList<Node> Relations = new ArrayList<Node>();
 
-    /* Overload constructor method to instantiate empty node to 
-     * return in Node.getParent() incase of null parent node. */
+    /* Overload constructor method to instantiate empty node to
+     * return in Node.getParents() incase of null Parents node. */
     public Node() {
         this.name = null;
         this.gender = null;
-        this.Parent = null;
+        this.Parents = new Node[2];
     }
-    // Main Constructor to use 
+    // Main Constructor to use
     public Node(String name, Gender gender) {
         this.name = name;
         this.gender = gender;
-        this.Parent = null;
+        this.Parents = new Node[2];
         System.out.println(this.name + ", " + this.gender);
     }
 
     // Logic Functions
-    public void addRelation(Node Person) {
-        Person.addParent(this);
-        this.Relations.add(Person);
+    public void addRelation(Node person) {
+        person.addParents(this);
+        this.Relations.add(person);
     }
-    private void addParent(Node Parent) {
-        this.Parent = Parent;
+    private void addParents(Node parent) {
+        if(parent.getGender() == Gender.Female) {
+            this.Parents[1] = parent;
+        } else {
+            this.Parents[0] = parent;
+        }
     }
 
     // Getters for data
     public String getName() {
-        return this.name;
+        if(name != null && !name.isEmpty()) {
+            return this.name;
+        }
+        return "Null name";
     }
     public Gender getGender() {
         return this.gender;
     }
-    public Node getParent() {
-        if (this.Parent == null) {
-            /* Garbage collector will eventually deallocate memory for non-referenced
-             * object, should manually run Runtime.GetRuntime().gc() to make sure. */
+    public Node getParent(Gender gender) {
+        if(this.Parents[1] == null && this.Parents[0] == null) {
             return new Node();
-        } else {
-            return this.Parent;
         }
+        if(gender == Gender.Female) {
+            return this.Parents[1];
+        } else if(gender == Gender.Male) {
+            return this.Parents[0];
+        } else return new Node();
+
     }
     public ArrayList<Node> getRelationships() {
         return this.Relations;
@@ -61,8 +70,9 @@ class App {
         Node Aaron = new Node("Aaron Baratheon", Gender.Male);
         Connor.addRelation(Ashley);
         Connor.addRelation(Aaron);
-        System.out.println("Get Connor's Parent Node: "+Connor.getParent().getName());
-        System.out.println("Get Ashley's Parent Node: "+Ashley.getParent().getName());
+
+        System.out.println("Get Connor's Parents Node: "+Connor.getParent(Gender.Male).getName());
+        System.out.println("Get Ashley's Parents Node: "+Ashley.getParent(Gender.Male).getName());
         System.out.print(Connor.getName()+" relationships: ");
         for (int i = 0; i < Connor.getRelationships().size(); i++) {
             Node Obj = Connor.getRelationships().get(i);
